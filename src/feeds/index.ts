5ff9273cd1a869cef371531afd9c7edf15b238eb
Eric Sakmar <eric.sakmar@gmail.com>
2026-07-20 15:49:40 -0400

    removes a feed i wasn't using

--- src/feeds/index.ts
+++ src/feeds/index.ts
@@ -2,4 +2,7 @@ import type { FeedGenerator } from "./types.js";
 import { bandcampRadioFeed } from "./bandcamp-radio.js";
 import { reverbBassGuitarsFeed } from "./reverb-bass-guitars.js";
 
-export const feeds: FeedGenerator[] = [bandcampRadioFeed, reverbBassGuitarsFeed];
+export const feeds: FeedGenerator[] = [
+  bandcampRadioFeed,
+  // reverbBassGuitarsFeed,
+];
